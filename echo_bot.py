dc8c1ba72e2473c6b29820b2564b4045f51813d4
Manuel Acosta Faerman <manuelacostafaerman@gmail.com>
2021-06-02 12:41:41 +0300

    prueba12

--- echo_bot.py
+++ echo_bot.py
@@ -22,6 +22,10 @@ def start_message(message):
 	if "office" in text_from_user:
 		bot.send_message(message.chat.id,f"Nuestra oficina se encuentra en Carretera de Canillas, 138, {message.chat.id}")
 	bot.send_location(chat_id, latitud=40.465297616884314,longitud=-3.6397970886211115)
+
+@bot.message_handler(func=lambda x: True)
+def start_message1(message):
+	text_from_user = message.json["text"]
 	if "contacto" or "mail" in text_from_user:
 		bot.send_message(chat_id, "estos son nuestros Email de contacto")
 		content = os.listdir(path)
