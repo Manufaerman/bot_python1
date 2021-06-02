d684255799f3d55e9e8b34739ee04bd3bbbee7b7
Manuel Acosta Faerman <manuelacostafaerman@gmail.com>
2021-06-02 06:43:38 +0300

    prueba4

--- echo_bot.py
+++ echo_bot.py
@@ -39,7 +39,8 @@ def send_welcome(message):
 def start_message(message):
 	text_from_user = message.json["text"]
 	if "office" in text_from_user:
-		bot.send_message(message.chat.id,bot.send_location(chat_id, 40.465297616884314, -3.6397970886211115))
+		bot.send_message(message.chat.id,f"ggigiu, {message.chat.id}")
+		bot.send_location(chat_id, latitud=40.465297616884314, longitud=-3.6397970886211115)
 
 
 
